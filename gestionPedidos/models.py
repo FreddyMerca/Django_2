bd1a194703f455be22b806bd23f8f26ca10f819b
Freddy Mercado <frmercado@emcali.com.co>
2024-10-07 15:27:30 -0500

    071024-1527

--- gestionPedidos/models.py
+++ gestionPedidos/models.py
@@ -8,13 +8,17 @@ class Clientes(models.Model):
     email=models.EmailField()
     telefono=models.CharField(max_length=10)
 
-
+    
 class Articulos(models.Model):
 
     nombre=models.CharField(max_length=30)
     seccion=models.CharField(max_length=30)
     precio=models.IntegerField()
 
+    def __str__(self):
+
+        return"El nombre es %s la seccion es %s y el precio es %s" %(self.nombre,self.seccion,self.precio)
+    
 class Pedidos(models.Model):
     numero=models.IntegerField()
     fecha=models.DateField
